af4adef89435e62bdb6b16026463b4769f7aae6f
Austin-Cathey <austincatheyart@gmail.com>
2024-02-27 18:15:57 -0500

    frontend updates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,11 +2,13 @@
 import { ref } from "vue";
 import ListOfHabits from "./components/ListOfHabits.vue";
 import Login from "./components/Login.vue";
+import Signup from "./components/Signup.vue";
 </script>
 
 <template>
   <div id="app">
     <Login />
+    <Signup />
     <h1 class="header">My Habits</h1>
     <ListOfHabits />
   </div>
--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,7 +1,35 @@
 <script setup>
 import { ref } from "vue";
+const nameOfUser = ref("");
+const userEmail = ref("");
+const password = ref("");
+const confirmPassword = ref("");
 </script>
 
-<template></template>
+<template>
+  <div>
+    <h3>New User Signup</h3>
+    <h5>
+      "Truth is, habits take longer than 21 days to create. Embrace the journey
+      with Beyond 21 Habit Tracker and transform your habits into lasting
+      lifestyle changes. Redefine habits. Achieve success."
+    </h5>
+    <form id="signupContainer" @submit.prevent="initiateSignup">
+      <input v-model="userName" type="text" placeholder="Create Username" />
+      <br />
+      <input v-model="userEmail" type="text" placeholder="Enter Email" />
+      <br />
+      <input v-model="password" type="text" placeholder="Create Password" />
+      <br />
+      <input
+        v-model="confirmPassword"
+        type="text"
+        placeholder="Confirm Password"
+      />
+      <br />
+      <button class="signinBtn" type="submit">Create Profile</button>
+    </form>
+  </div>
+</template>
 
 <style></style>
