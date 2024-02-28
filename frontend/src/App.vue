85f937880073f3ed33b4095fa568a380b9afedaf
Austin-Cathey <austincatheyart@gmail.com>
2024-02-28 13:35:48 -0500

    got routers set up for home, login, and signup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { ref } from "vue";
 import ListOfHabits from "./components/ListOfHabits.vue";
-import Login from "./components/Login.vue";
-import Signup from "./components/Signup.vue";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Signup from "./views/Signup.vue";
 </script>
 
 <template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/signup">Signup</router-link>
+  </div>
+  <p>
+    <!-- <button @click="$router.go(-1)">Go back 1 step</button>
+    <button @click="$router.go(1)">Go forward 1 step</button> -->
+
+    <!--     <button @click="$router.push('/user/1')">Redirect to "/user/1"</button>
+ -->
+  </p>
+  <router-view />
   <div id="app">
     <Login />
     <Signup />
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,28 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+    {
+path: '/',
+name: 'Home', 
+component: () => import('./views/Home.vue')
+    },
+    {
+path: '/login',
+name: 'Login',
+component: () => import('./views/Login.vue')
+},
+{
+    path: '/signup',
+    name: 'Signup',
+    components: () => import('./views/Signup.vue') 
+},]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
