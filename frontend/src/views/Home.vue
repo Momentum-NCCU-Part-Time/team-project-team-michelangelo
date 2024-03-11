c446e39f396b0eefbe4c33683eb7d9d1c443a5ed
Austin-Cathey <austincatheyart@gmail.com>
2024-03-11 11:05:52 -0400

    added signup login buttons to home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,16 @@
 <script setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
 </script>
 
 <template>
   <h1>Welcome to Beyond 21, please sign up or login</h1>
+  <button @click="router.push('/login')" class="signUpBtn" type="submit">
+    User Login
+  </button>
+  <button @click="router.push('/signup')" class="signUpBtn" type="submit">
+    New User Signup
+  </button>
 </template>
 <style></style>
