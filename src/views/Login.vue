fbdd8c7bd36e6ef13d20a3f97c10ec294acc6aca
Megan <megancgeo@gmail.com>
2024-03-17 14:03:34 -0400

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -26,7 +26,7 @@ const initiateLogin = () => {
     .then((data) => {
       console.log(data.auth);
       /* loggedIn.value = true; */
-      token = data.auth;
+      token = $2a$10$JmBIWzs3QmerO0BbnAi8hO8WKy6DOVmcFVOLRvwCTuFadPfUsGj8y;
       localStorage.setItem("token", JSON.stringify(token));
       /* state.token = data.auth; */
       router.push("/habits");
@@ -39,18 +39,53 @@ const initiateLogin = () => {
 
 <template>
   <div>
-    <h3>Beyond21 Login</h3>
+ <br />
+    <img src="./logo.png" padding="-20px" width="220px" />
+    <br />
+    <br />
+
     <form id="loginContainer" @submit.prevent="initiateLogin">
-      <input v-model="userEmail" type="text" placeholder="Email" />
+      <label Email for="userName"> Email (case-sensitive) </label>
+      <input
+        v-model="userEmail"
+        type="text"
+        placeholder="Email (case-sensitive)"
+        class="form-control"
+      />
       <br />
-      <input v-model="password" type="password" placeholder="Password" />
+      <label Email for="password"> Password </label>
+      <a class="float-right forgot-password"> Forgot Password? </a>
+      <input
+        v-model="passwordInput"
+        type="password"
+        placeholder="Password"
+        class="form-control"
+      />
+      <br />
+
       <br />
       <button class="loginBtn" type="submit">Login</button>
       <br />
-      <button @click="router.push('/signup')" class="signUpBtn" type="submit">
-        New User Signup
-      </button>
+
+      <div class="strike"></div>
+      <h2>OR</h2>
+      <sup>Don't have a Beyond 21 account?</sup>
+      <br />
+      <button class="signUpBtn" type="submit">New User Sign up</button>
     </form>
+  </div>
+
+  <div class="made">
+    <p>
+      Built during Momentum Team NCCU 1 Cohort in &#128002 Durham, N.C. <br />by 
+      <a href="https://LinkedIn.com/in/joannaem">Joanna Matanga </a>,
+      <a href="https://linkedin.com/in/austin-cathey"> Austin Cathey </a>, 
+      <a href="https://linkedin.com/in/megancgeo"> Megan Cain </a> & 
+      <a href="https://www.linkedin.com/in/sanadevkota"> Upasana Devkota</a>
+    </p>
+
+
+
   </div>
 </template>
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -36,27 +36,67 @@ const addNewUser = () => {
 
 <template>
   <div>
-    <h3>New User Signup</h3>
-    <h5>
-      "Truth is, habits take longer than 21 days to create. Embrace the journey
-      with Beyond 21 Habit Tracker and transform your habits into lasting
-      lifestyle changes. Redefine habits. Achieve success."
-    </h5>
-    <form id="signupContainer" @submit.prevent="addNewUser">
-      <input v-model="userName" type="text" placeholder="Create Username" />
-      <br />
-      <input v-model="userEmail" type="text" placeholder="Enter Email" />
-      <br />
-      <input v-model="password" type="password" placeholder="Create Password" />
-      <br />
-      <!-- <input
-        v-model="confirmPassword"
+  <br />
+    <img src="./logo.png" padding="-20px" width="220px" />
+    <br />
+    <br />
+
+    
+      <h3> Truth is, habits take longer <br />than 21 days to create.</h3>
+      <h4>Embrace the journey
+      with Beyond 21 and transform your habits into lasting
+      lifestyle changes.</h4>
+    <br /> <br />
+
+    <form id="signupContainer" @submit.prevent="initiateSignup">
+      <label Username for="UserName"> Username </label>
+      <input
+        v-model="userName"
         type="text"
-        placeholder="Confirm Password"
+        placeholder="e.g., Luuuke"
+        class="form-control"
       />
-      <br /> -->
-      <button class="signinBtn" type="submit">Create Profile</button>
+      <br />
+
+      <label Email for="userEmail"> Email</label>
+      <input
+        v-model="userEmail"
+        type="text"
+        placeholder="e.g., michelangelo@example.com"
+        class="form-control"
+      />
+      <br />
+
+      <label Password for="password"> Password</label>
+      <input
+        v-model="password"
+        type="text"
+        placeholder="e.g., *******************"
+        class="form-control"
+      />
+      <br />
+     
+      <p><sup> By signing up you are accepting Beyond21's<br/> Terms and Conditions and Privacy Policy</sup> </p>
+      <button class="signinBtn" type="submit">Sign up</button>
+
+      <div class="strike"></div>
+
+      <h2>OR</h2>
+      <sup>Already have a Beyond21 account?</sup>
+      <br />
+      <button class="loginBtn" type="submit">Login</button>
+      <br />
     </form>
+  </div>
+
+  <div class="made">
+     <p>
+      Built during Momentum Team NCCU 1 Cohort in &#128002 Durham, N.C. <br />by 
+      <a href="https://LinkedIn.com/in/joannaem">Joanna Matanga </a>,
+      <a href="https://linkedin.com/in/austin-cathey"> Austin Cathey </a>, 
+      <a href="https://linkedin.com/in/megancgeo"> Megan Cain </a> & 
+      <a href="https://www.linkedin.com/in/sanadevkota"> Upasana Devkota</a>
+    </p>
   </div>
 </template>
 
